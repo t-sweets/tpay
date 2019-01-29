480bacfc05b343bbb3485f749d898367c7ae25e9
Shota Harikae <shota@sorahane.me>
2019-01-29 18:07:28 +0900

    init jwt

--- tpay/settings.py
+++ tpay/settings.py
@@ -100,3 +100,15 @@ STATIC_URL = '/static/'
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
 )
+
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}

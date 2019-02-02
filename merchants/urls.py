534cb3b906f8b0d606c91aaf5365c8d92b2773ac
Shota Harikae <shota@sorahane.me>
2019-02-03 00:10:58 +0900

    mod urls

--- merchants/urls.py
+++ merchants/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+
 from .views import MerchantViewSet, MerchantAllCheckoutListView, MerchantCheckoutListView
 
 
 urlpatterns = [
-    url(r'^checkout/', MerchantAllCheckoutListView.as_view()),
+    url(r'^', MerchantViewSet.as_view({'get': 'list'})),
+    url(r'^(?P<merchant>.+)/$', MerchantViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    url(r'^checkout/$', MerchantAllCheckoutListView.as_view()),
     url(r'^(?P<merchant>.+)/checkout/$', MerchantCheckoutListView.as_view()),
 ]

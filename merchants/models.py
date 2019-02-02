4a4d040161c627d54cedb1918fdb41b28cf8fb7b
Shota Harikae <shota@sorahane.me>
2019-02-02 23:20:58 +0900

    mod merchant models

--- merchants/models.py
+++ merchants/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 import uuid as uuid_lib
 
+from accounts.models import Account
+
 
 class Merchant(models.Model):
     uuid = models.UUIDField(default=uuid_lib.uuid4, unique=True, editable=False)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
     name = models.CharField(_('merchant name'), max_length=30)
+    accounts = models.ManyToManyField(Account)
 
     def __str__(self):
         return self.name

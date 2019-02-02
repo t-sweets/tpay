1f99b5fdd7e3f532b1884e8a803ede84f6949c0b
Shota Harikae <shota@sorahane.me>
2019-02-02 20:59:45 +0900

    add merchant models

---
+++ merchants/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+import uuid as uuid_lib
+
+
+class Merchant(models.Model):
+    uuid = models.UUIDField(default=uuid_lib.uuid4, unique=True, editable=False)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    name = models.CharField(_('merchant name'), max_length=30)
+
+    def __str__(self):
+        return self.name

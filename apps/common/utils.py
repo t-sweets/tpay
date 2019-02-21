4af21d9028dd56a13ca250f71930521a2d93a6ae
Shota Harikae <shota@sorahane.me>
2019-02-21 17:23:18 +0900

    fixd

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -9,4 +9,7 @@ def t_pay_exception_handler(exc, context):
     if isinstance(exc, ValueError):
         content = {'detail': '{}'.format(exc.args)}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    elif isinstance(exc, KeyError):
+        content = {'detail': '{}'.format(exc.args)}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
     return response
--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^jet/', include('jet.urls', 'jet')),
-    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
     url(r'^api/', include([
         url(r'^v1/', include([

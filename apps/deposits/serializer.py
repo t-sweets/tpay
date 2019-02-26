9a9343548e7a6395b6bab76556780e0276845652
Shota Harikae <shota@sorahane.me>
2019-02-26 15:27:01 +0900

    add merchant validate

--- apps/deposits/serializer.py
+++ apps/deposits/serializer.py
@@ -11,4 +11,9 @@ class DepositSerializer(serializers.ModelSerializer):
         fields = ('id', 'created_time', 'updated_time', 'amount', 'idm')
 
     def create(self, validated_data):
+        validated_data['merchant'] = validated_data.pop('merchant_id', None)
+        if validated_data['merchant'] is None:
+            raise serializers.ValidationError("merchant not found.")
+        if self.context['request'].user not in validated_data['merchant'].accounts.all():
+            raise serializers.ValidationError("merchant not in auth user.")
         return Deposit.objects.deposit(request_data=validated_data)

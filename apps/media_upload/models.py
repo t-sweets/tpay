87da968b12e956f8be5c523ad6aacf3376bb73b3
Shota Harikae <shota@sorahane.me>
2019-03-01 03:20:10 +0900

    add image model

---
+++ apps/media_upload/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+import uuid
+import os
+from django.utils.translation import gettext_lazy as _
+
+
+class Image(models.Model):
+    def get_image_path(self, filename):
+        prefix = 'images/'
+        name = uuid.uuid4().hex
+        extension = os.path.splitext(filename)[-1]
+        return prefix + name + extension
+
+    image = models.ImageField(
+        upload_to=get_image_path,
+        verbose_name=_('image'),
+        height_field='height',
+        width_field='width',
+    )
+
+    height = models.IntegerField(
+        editable=False,
+    )
+
+    width = models.IntegerField(
+        editable=False,
+    )

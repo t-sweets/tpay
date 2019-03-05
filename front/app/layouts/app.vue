208a22b61f2450f56db53302e7af7e7bb3497649
sho <sskmy1024.y@gmail.com>
2019-03-05 20:22:20 +0900

    add fixed passive

--- front/app/layouts/app.vue
+++ front/app/layouts/app.vue
@@ -24,6 +24,38 @@ export default {
     }
   },
   created() {
+    /* "passive" が使えるかどうかを検出 */
+    var passiveSupported = false;
+    try {
+      document.addEventListener(
+        "test",
+        null,
+        Object.defineProperty({}, "passive", {
+          get: function() {
+            passiveSupported = true;
+          }
+        })
+      );
+    } catch (err) {}
+
+    /* リスナーを登録 */
+    document.addEventListener(
+      "touchstart",
+      function listener(e) {
+        /* do something */
+        e.preventDefault();
+      },
+      passiveSupported ? { passive: false } : false
+    );
+    document.addEventListener(
+      "touchmove",
+      function listener(e) {
+        /* do something */
+        e.preventDefault();
+      },
+      passiveSupported ? { passive: false } : false
+    );
+
     if (this.$ons.platform.isIPhoneX()) {
       document.documentElement.setAttribute("onsflag-iphonex-portrait", "");
     }

576566066bd6d8697ccec596d4bf2834f68026f2
sho <sskmy1024.y@gmail.com>
2019-03-12 22:22:15 +0900

    fixed param

--- front/app/layouts/app.vue
+++ front/app/layouts/app.vue
@@ -25,7 +25,7 @@ export default {
   },
   mounted() {
     const html = document.documentElement;
-    if (this.$ons.platform.isIPhoneX() && this.$route.params.standalone) {
+    if (this.$ons.platform.isIPhoneX() && this.$route.query.standalone) {
       html.setAttribute("onsflag-iphonex-portrait", "");
     }
     document.addEventListener(

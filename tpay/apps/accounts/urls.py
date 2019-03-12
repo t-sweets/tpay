4bf47e9809b3da5ce2ea2ae07a2ab6fe24acea86
Shota Harikae <mail@havicon.net>
2019-03-13 04:20:19 +0900

    fixd idm delete and **

--- tpay/apps/accounts/urls.py
+++ tpay/apps/accounts/urls.py
@@ -9,12 +9,14 @@ urlpatterns = [
 
     url(r'^profile/$', AccountViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy', 'patch': 'partial_update'})),
 
-    url(r'^idm/$', IdmView.as_view({'post': 'create',
-                                    'get': 'list',
-                                    'put': 'update',
-                                    'delete': 'destroy',
-                                    'patch': 'partial_update'})),
+    url(r'^idms/$', IdmView.as_view({'post': 'create',
+                                     'get': 'list'})),
+
+    url(r'^idms/(?P<pk>.+)/$', IdmView.as_view({'put': 'update',
+                                                'delete': 'destroy',
+                                                'patch': 'partial_update'})),
+
 
     url(r'^checkouts/$', AccountCheckoutViewSet.as_view({'get': 'list'})),
-    url(r'^checkouts/(?P<id>.+)/$', AccountCheckoutViewSet.as_view({'get': 'retrieve'})),
+    url(r'^checkouts/(?P<pk>.+)/$', AccountCheckoutViewSet.as_view({'get': 'retrieve'})),
 ]

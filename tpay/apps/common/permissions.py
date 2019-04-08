ab010ca3e2a6288041ad464c94cddae58344e0f4
Shota Harikae <shota@sorahane.me>
2019-04-08 19:20:56 +0900

    add is merchant owner

---
+++ tpay/apps/common/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class IsMerchantOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user in obj.merchant.accounts.all()

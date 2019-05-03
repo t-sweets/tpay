f30e39612e459c729c607fc84c1f228623a731f0
sharikae <st.harikae@gmail.com>
2019-05-04 02:36:48 +0900

    only enable ldap prod

--- tpay/config/settings/production.py
+++ tpay/config/settings/production.py
@@ -14,3 +14,31 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# LDAP Settings
+AUTHENTICATION_BACKENDS = (
+    "django_python3_ldap.auth.LDAPBackend",
+)
+
+LDAP_AUTH_URL = "ldap://192.168.189.4"
+LDAP_AUTH_USE_TLS = False
+LDAP_AUTH_SEARCH_BASE = "ou=people,dc=t-lab,dc=cs,dc=teu,dc=ac,dc=jp"
+LDAP_AUTH_USER_FIELDS = {
+    "username": "uid",
+    "email": "mail",
+}
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django_python3_ldap": {
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
+    },
+}
